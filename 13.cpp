8d4034503340b848cedab80445c57ea104c6b352
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2022-08-31 10:32:13 +0530

    Add files via upload

---
+++ 13.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout<<"\n-----------------------------------------\n";
+    cout<<"Program to print ASCII value of character\n";
+    cout<<"-----------------------------------------\n";
+
+    cout<<"\nEnter the character :- ";
+    cin>>ch;
+
+    cout<<"\nASCII value of character is :- "<<int(ch)<<"\n";
+
+    return 0;
+
+}
