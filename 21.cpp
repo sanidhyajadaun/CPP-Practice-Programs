eff41b47da7a49899027f16ebb479a69a9212f71
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2022-09-04 16:39:21 +0530

    Add files via upload

---
+++ 21.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"-----------Program to display the size of different datatype-----------";
+    cout<<"\nSize of Int :- "<<sizeof(int)<<" bytes";
+    cout<<"\nSize of Float :- "<<sizeof(float)<<" bytes";
+    cout<<"\nSize of Double :- "<<sizeof(double)<<" bytes";
+    cout<<"\nSize of Char :- "<<sizeof(char)<<" bytes";
+    return 0;
+}
