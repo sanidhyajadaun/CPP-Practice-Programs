091f5bd67ada09232a14a2e9ff54665ff092b2d6
Sanidhya Jadaun <77571526+sanidhyajadaun@users.noreply.github.com>
2022-10-09 21:57:37 +0530

    Add files via upload

---
+++ 40.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+/*
+//void swap(int *x,int *y)
+//{
+    int temp = *x;
+        *x = *y;
+        *y = temp;
+}
+*/
+void selection_sort(int arr[],int size)
+{
+    int i,j,min_index;
+
+    //one by one moving to unsorted array
+    for(i=0;i<size-1;i++)
+    {
+        //finde the min element in o=unsorted array
+        min_index=i;
+        for (j = 0; j < size; j++)
+        {
+            if (arr[j]<arr[min_index])
+            {
+                min_index=j;
+            }
+
+            //swap the found min element with first element
+            if(min_index!=i)
+            {
+                swap(&arr[min_index],&arr[i]);
+            }
+            
+        }
+        
+    }
+
+}
+
+
+//Function to print an array
+void print_array(int arr[],int size)
+{
+    for(int i=0;i<size;i++)
+    {
+        cout<<arr[i]<<"  ";
+    }
+}
+
+
+int main()
+{
+    int i,arr[50],n;
+
+    cout<<"\n\n---------------------------------------";
+    cout<<"\nProgram to sort array using Selection sort\n";
+    cout<<"---------------------------------------\n\n";
+
+    cout<<"Enter the size n of the array :- ";
+    cin>>n;
+
+    cout<<"\n\nEnter the elements of the array :- \n";
+    for (i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    cout<<"\n\nArray before sorting :- ";
+    print_array(arr,n);
+
+    selection_sort(arr,n);
+    cout<<"\n\nArray After Soritng :- ";
+    print_array(arr,n);
+
+    return 0;
+    
+
+}
